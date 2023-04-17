2b839be84a8431816eb90f334f1bce31e76233dd
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-04-17 17:32:40 +0400

    Add files via upload

--- ProductBaskt/assets/js/basket.js
+++ ProductBaskt/assets/js/basket.js
@@ -3,46 +3,69 @@ function FetchItems() {
     let alertMessage = document.querySelector('.alert');
 
     if (itemList.length === 0) {
-        alertMessage.classList.remove('d-none')
-        document.querySelector('table').classList.remove('d-none')
-    }
-    else {
-
-
-        alertMessage.classList.add('d-none')
+        alertMessage.classList.remove('d-none');
+        document.querySelector('table').classList.remove('d-none');
+    } else {
+        alertMessage.classList.add('d-none');
 
         let x = '';
         itemList.forEach(item => {
             x += `
-    <tr>
-        <th scope="row">${item.Id}</th>
-        <td>
-            <img src=${item.Image}>
-        </td>
-        <td>${item.Model}</td>
-        <td>
-            <input id="countvalue" type="number" min="1" value=${item.Count}>
-        </td>
-        <td>${item.Price}<strong>$</strong></td>
-
-        <td>${(parseInt(item.Price)) * parseInt(item.Count)} USD</td>
-        <td>
-            <button class="btn btn-danger">Remove</button>
-        </td>
-    </tr>
-    `
-        })
+          <tr>
+            <th id="${item.Id}" scope="row">${item.Id}</th>
+            <td>
+              <img src=${item.Image}>
+            </td>
+            <td>${item.Model}</td>
+            <td>
+              <input id="countvalue" type="number" min="1" value=${item.Count}>
+            </td>
+            <td>${item.Price}<strong>$</strong></td>
+            <td>${(parseInt(item.Price)) * parseInt(item.Count)} USD</td>
+            <td>
+              <button class="btn btn-danger">Remove</button>
+            </td>
+          </tr>
+        `;
+        });
         document.querySelector('tbody').innerHTML = x;
 
+        let button = document.querySelectorAll('.btn-danger');
 
+        for (let btn of button) {
+            btn.onclick = function () {
+                let id = parseInt(this.parentElement.parentElement.firstChild.id);
+                let itemList = JSON.parse(localStorage.getItem('products'));
+                itemList.splice(id - 1, 1);
+                localStorage.setItem('products', JSON.stringify(itemList));
+                FetchItems();
+                showCount();
+                window.location.reload()
+            }
+        }
+    }
+}
+
+FetchItems();
+
+let countvalues = document.querySelectorAll('#countvalue');
+
+for (let i = 0; i < countvalues.length; i++) {
+    countvalues[i].addEventListener('input', changeCount);
+}
+
+function changeCount() {
+    let itemList = JSON.parse(localStorage.getItem('products'));
+
+    for (let i = 0; i < countvalues.length; i++) {
+        let count = parseInt(countvalues[i].value);
+        itemList[i].Count = count;
     }
 
+    localStorage.setItem('products', JSON.stringify(itemList));
+
+    window.location.reload();
 }
-FetchItems()
-
-
-    
-  
 
 
 
--- ProductBaskt/assets/js/main.js
+++ ProductBaskt/assets/js/main.js
@@ -1,22 +1,23 @@
 if (!localStorage.getItem('products')) {
-    localStorage.setItem('products', JSON.stringify([]));
-  }
-  
-  let buttons = document.querySelectorAll('.btn');
-  
-  for (let btn of buttons) {
-    btn.addEventListener('click', function(e) {
-      e.preventDefault();
-  
+  localStorage.setItem('products', JSON.stringify([]));
+}
+
+let buttons = document.querySelectorAll('.btn');
+
+for (let btn of buttons) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (this.previousElementSibling !== null) {
       let id = this.parentElement.parentElement.id;
       let model = this.previousElementSibling.innerHTML;
       let price = this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
       let img = this.parentElement.previousElementSibling.src;
-  
+
       let allproducts = JSON.parse(localStorage.getItem('products'));
-  
+
       let existProd = allproducts.find(item => item.Id === parseInt(id));
-  
+
       if (existProd === undefined) {
         allproducts.push({
           Id: parseInt(id),
@@ -25,20 +26,28 @@ if (!localStorage.getItem('products')) {
           Model: model,
           Price: price,
         });
+        document.querySelector('.toaster').innerHTML = 'Successfully added!'
+        document.querySelector('.toaster').style.backgroundColor = 'green'
       } else {
         existProd.Count += 1;
+        document.querySelector('.toaster').innerHTML = 'Already added!'
+        document.querySelector('.toaster').style.backgroundColor = 'red'
       }
-  
-      localStorage.setItem('products', JSON.stringify(allproducts));
-  
-      showCount();
-    });
-  }
-  
-  function showCount() {
-    let products = JSON.parse(localStorage.getItem('products'));
-    document.querySelector('sup').innerHTML = products.length;
-  }
-  showCount();
 
-  
+      document.querySelector('.toaster').style.right = '5%'
+      setTimeout(() => {
+        document.querySelector('.toaster').style.right = '-20%'
+      }, 1300);
+
+      localStorage.setItem('products', JSON.stringify(allproducts));
+      showCount();
+    }
+  });
+}
+
+function showCount() {
+  let products = JSON.parse(localStorage.getItem('products'));
+  document.querySelector('sup').innerHTML = products.length;
+}
+
+showCount();
